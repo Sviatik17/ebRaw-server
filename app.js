b71c63ed4932946af3b57dcd9e7c7f7341bdec2d
Sviatoslav <107312413+Sviatik17@users.noreply.github.com>
2025-04-27 15:41:23 +0300

    database connected

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+ require('dotenv').config();
+
+const MONGO_URI = process.env.MONGO_URI;
+
 const app = express();
+
+
+mongoose.connect(MONGO_URI, )
+.then(() => {
+    console.log('connected to database');
+})
+.catch(() => {
+    console.log('connection failed');
+});
+
 
 app.get('/', (req, res) => { res.send('Hello World!');
 }); 
@@ -7,3 +22,5 @@ app.get('/', (req, res) => { res.send('Hello World!');
 app.listen(3000, () => {
     console.log('server is running on port 3000!');
 })
+
+// g1IlMmK5HUnaiSLH
